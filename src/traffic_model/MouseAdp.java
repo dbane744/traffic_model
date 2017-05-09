064b102c7286bdbb093368391e6dd0e7587b3642
dbane744 <aaaoooaaa@hotmail.co.uk>
2017-05-09 21:57:18 +0100

    Versions 1.0

--- src/traffic_model/MouseAdp.java
+++ src/traffic_model/MouseAdp.java
@@ -1,3 +1,20 @@
+/**
+ *Copyright 2017 Daniel Bane
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package traffic_model;
 
 import java.awt.Dimension;
@@ -9,116 +26,120 @@ import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-
-
 /**
- * Implements a mouse listener on the model panel.
- * Grabs the location where the mouse was clicked to allow the placement of cars or traffic lights.
- * Left click on or to the side of the road places a car. Right click places a traffic light.
+ * Implements a mouse listener for the model panel. 
+ * 
+ * Grabs the location where the
+ * mouse was clicked to allow the placement of cars or traffic lights. Left
+ * click on or to the side of the road places a car. Right click places a
+ * traffic light.
+ * 
  * @author Daniel Bane
  *
  */
-public class MouseAdp extends MouseAdapter{
-  
-  /**
-   * Stores the model panel.
-   */
-  JPanel mPanel;
-  
+public class MouseAdp extends MouseAdapter {
 
-  public MouseAdp(JPanel mPanel){
-    this.mPanel = mPanel;
-  }
-  
-  /**
-   * The listener for mouse clicks. A left click on the road will place a vehicle. A right click will place a traffic light.
-   */
-  public void mouseClicked(MouseEvent me){
-    
-    // Gets the current instance of temporaryMap in Storage.
-    double[][] tempMap = Storage.getInstance().getTempMap();
-    
-    // Only listens for clicks if there is an image loaded in the panel and the model is not running.
-    if(tempMap != null && !Storage.getInstance().getRuntime()){
-    
-    // Gets the current panel size.   
-    Dimension currentpanelSize = mPanel.getSize();
-    int panelX =  (int)currentpanelSize.getHeight();
-    int panelY = (int)currentpanelSize.getWidth();
-    
-    // Gets the image size.
-    BufferedImage image = Storage.getInstance().getDataAsImage();
-    int imageX = image.getHeight();
-    int imageY = image.getWidth();
-    
-    // Gets the ratio between the image size and the panel size.
-    double ratio = (double)imageY / (double)panelY;
-    
-      
-    // Returns the point that was clicked. And stores the x and y values for further use.
-    Point clickedPoint = me.getPoint();
-   
-    
-      // The raw x and y values are rescaled to the ratio of the image -
-      // allows the panel/image to be larger than the inputed image while
-      // allowing the user to
-      // select a coordinate within the original image.
+	/**
+	 * Stores the model panel.
+	 */
+	JPanel mPanel;
 
-    int x = (int) (clickedPoint.getX() * ratio);
-    int y = (int) (clickedPoint.getY() * ratio);
-    
-      // Adds a vehicle to temporaryMap (which is displayed in the panel) if the
-      // user left clicks on the road.
-      if (SwingUtilities.isLeftMouseButton(me)) {
-        // Only places if the clicked tile is a road.
-        if (tempMap[y][x] <= 4 && tempMap[y][x] > 0) {
-          // Adds 9 to the road element to signify a vehicle.
-          tempMap[y][x] = (tempMap[y][x]) + 9;
-          // Replaces the map in storage with the new map.
-          Storage.getInstance().setTempMap(tempMap);
-          mPanel.repaint();
-        }
-      }
-    
-    
-    // Places a traffic light if the user right clicks near a road.
-    else if(SwingUtilities.isRightMouseButton(me)){
+	public MouseAdp(JPanel mPanel) {
+		this.mPanel = mPanel;
+	}
 
-      if(tempMap[y][x] <= 4 && tempMap[y][x] > 0) {
-        // Adds 100 to the road element to signify a traffic light.
-        tempMap[y][x] = (tempMap[y][x]) + 100;
-        
-        // If the traffic light is on a north or south road tiles will be altered for painting to the east and west.
-        if(tempMap[y][x] == 101 || tempMap[y][x] == 103){
-          // In case the traffic light is placed on a corner, only paints to empty spaces.
-          if(tempMap[y][x+1] == 0){
-            // 200 signifies the adjacently painted tiles.
-            tempMap[y][x+1] = 200;
-          }
-          if(tempMap[y][x-1] == 0){
-            tempMap[y][x-1] = 200;
-          }
-        }
-        
-        // If the traffic light is on an east or west road tiles will be altered for painting to the north and south.
-        if(tempMap[y][x] == 102 ||tempMap[y][x] == 104){
+	/**
+	 * The listener for mouse clicks. A left click on the road will place a
+	 * vehicle. A right click will place a traffic light.
+	 */
+	public void mouseClicked(MouseEvent me) {
 
-          if(tempMap[y+1][x] == 0){
-            tempMap[y+1][x] = 200;
-          }
-          if(tempMap[y-1][x] == 0){
-            tempMap[y-1][x] = 200;
-          }
-        }
-        
-        // Replaces the map in storage with the new map.
-        Storage.getInstance().setTempMap(tempMap);
-        mPanel.repaint();
-      }
-    }
-    
-    }
-    
-  }
+		// Gets the current instance of temporaryMap in Storage.
+		double[][] tempMap = Storage.getInstance().getTempMap();
+
+		// Only listens for clicks if there is an image loaded in the panel and
+		// the model is not running.
+		if (tempMap != null && !Storage.getInstance().getRuntime()) {
+
+			// Gets the current panel size.
+			Dimension currentpanelSize = mPanel.getSize();
+			int panelX = (int) currentpanelSize.getHeight();
+			int panelY = (int) currentpanelSize.getWidth();
+
+			// Gets the image size.
+			BufferedImage image = Storage.getInstance().getDataAsImage();
+			int imageX = image.getHeight();
+			int imageY = image.getWidth();
+
+			// Gets the ratio between the image size and the panel size.
+			double ratio = (double) imageY / (double) panelY;
+
+			// Returns the point that was clicked. And stores the x and y values
+			// for further use.
+			Point clickedPoint = me.getPoint();
+
+			// The raw x and y values are rescaled to the ratio of the image -
+			// allows the panel/image to be larger than the inputed image while
+			// allowing the user to
+			// select a coordinate within the original image.
+
+			int x = (int) (clickedPoint.getX() * ratio);
+			int y = (int) (clickedPoint.getY() * ratio);
+
+			// Adds a vehicle to temporaryMap (which is displayed in the panel)
+			// if the
+			// user left clicks on the road.
+			if (SwingUtilities.isLeftMouseButton(me)) {
+				// Only places if the clicked tile is a road.
+				if (tempMap[y][x] <= 4 && tempMap[y][x] > 0) {
+					// Adds 9 to the road element to signify a vehicle.
+					tempMap[y][x] = (tempMap[y][x]) + 9;
+					// Replaces the map in storage with the new map.
+					Storage.getInstance().setTempMap(tempMap);
+					mPanel.repaint();
+				}
+			}
+
+			// Places a traffic light if the user right clicks near a road.
+			else if (SwingUtilities.isRightMouseButton(me)) {
+
+				if (tempMap[y][x] <= 4 && tempMap[y][x] > 0) {
+					// Adds 100 to the road element to signify a traffic light.
+					tempMap[y][x] = (tempMap[y][x]) + 100;
+
+					// If the traffic light is on a north or south road tiles
+					// will be altered for painting to the east and west.
+					if (tempMap[y][x] == 101 || tempMap[y][x] == 103) {
+						// In case the traffic light is placed on a corner, only
+						// paints to empty spaces.
+						if (tempMap[y][x + 1] == 0) {
+							// 200 signifies the adjacently painted tiles.
+							tempMap[y][x + 1] = 200;
+						}
+						if (tempMap[y][x - 1] == 0) {
+							tempMap[y][x - 1] = 200;
+						}
+					}
+
+					// If the traffic light is on an east or west road tiles
+					// will be altered for painting to the north and south.
+					if (tempMap[y][x] == 102 || tempMap[y][x] == 104) {
+
+						if (tempMap[y + 1][x] == 0) {
+							tempMap[y + 1][x] = 200;
+						}
+						if (tempMap[y - 1][x] == 0) {
+							tempMap[y - 1][x] = 200;
+						}
+					}
+
+					// Replaces the map in storage with the new map.
+					Storage.getInstance().setTempMap(tempMap);
+					mPanel.repaint();
+				}
+			}
+
+		}
+
+	}
 }
-
--- src/traffic_model/Storage.java
+++ src/traffic_model/Storage.java
@@ -1,3 +1,20 @@
+/**
+ *Copyright 2017 Daniel Bane
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package traffic_model;
 
 import java.awt.Color;
@@ -9,8 +26,9 @@ import java.awt.image.MemoryImageSource;
 import java.util.ArrayList;
 
 /**
- * Stores large data sets used by the application. Includes map/road network data and statistical results of the model.
- * This class is a singleton to allow easy access by multiple classes.
+ * Stores large data sets used by the application. Includes map/road network
+ * data and statistical results of the model. This class is a singleton to allow
+ * easy access by multiple classes.
  * 
  * @author Daniel Bane
  *
@@ -18,7 +36,7 @@ import java.util.ArrayList;
 public class Storage {
 
 	/**
-	 * Stores the instance of this class -  a singleton.
+	 * Stores the instance of this class - a singleton.
 	 */
 	private static Storage storage = null;
 
@@ -28,12 +46,14 @@ public class Storage {
 	private double[][] initialMap;
 
 	/**
-	 * Stores the current state of the model (changes constantly as the model is ran). Can be reset by the user to contain initialMap. 
+	 * Stores the current state of the model (changes constantly as the model is
+	 * ran). Can be reset by the user to contain initialMap.
 	 */
 	private double[][] temporaryMap;
 
 	/**
-	 * Is set to true when reset map is called. Tells the model vehicle movement loop to cancel.
+	 * Is set to true when reset map is called. Tells the model vehicle movement
+	 * loop to cancel.
 	 */
 	private boolean cancelModelLoop = false;
 	/**
@@ -45,16 +65,19 @@ public class Storage {
 	 */
 	private int numOfVehicles = 0;
 	/**
-	 * Stores whether the data is currently being altered (the model is running).
+	 * Stores whether the data is currently being altered (the model is
+	 * running).
 	 */
 	private boolean runtime = false;
 	/**
 	 * Stores the percentage of vehicles currently stood still.
+	 * 
 	 * @return The percentage as a double.
 	 */
 	private int percentStoodStill;
 	/**
-	 * Stores the average number of tiles each vehicle is from the vehicle in front.
+	 * Stores the average number of tiles each vehicle is from the vehicle in
+	 * front.
 	 */
 	private double averageVehicDist;
 	/**
@@ -66,19 +89,17 @@ public class Storage {
 	 */
 	private double overallDist = 0;
 
-
-
-	// Defeats instantiation 
-	private Storage(){
+	// Defeats instantiation
+	private Storage() {
 	}
 
 	/**
 	 * Grabs the instance of the class (as a singleton).
 	 * 
-	 * @return A link to this singleton. 
+	 * @return A link to this singleton.
 	 */
-	public static Storage getInstance(){
-		if(storage == null){
+	public static Storage getInstance() {
+		if (storage == null) {
 			storage = new Storage();
 		}
 		return storage;
@@ -86,21 +107,21 @@ public class Storage {
 
 	/**
 	 * Returns the boolean value of whether to cancel the vehicle move loop.
-	 * @return 
+	 * 
+	 * @return
 	 */
-	public boolean getCancelModelLoop(){
+	public boolean getCancelModelLoop() {
 		return cancelModelLoop;
 	}
 
 	/**
 	 * Sets whether to cancel the vehicle move loop.
+	 * 
 	 * @param value
 	 */
-	public void setCancelModelLoop(boolean value){
+	public void setCancelModelLoop(boolean value) {
 		cancelModelLoop = value;
 	}
-
-
 
 	/**
 	 * Sets the area map. Also resets the temporary map to the same data.
@@ -121,10 +142,12 @@ public class Storage {
 	}
 
 	/**
-	 * Sets the current state of the temporary map. 
-	 * @param inputMap The current state of the map.
+	 * Sets the current state of the temporary map.
+	 * 
+	 * @param inputMap
+	 *            The current state of the map.
 	 */
-	public void setTempMap(double[][] inputMap){
+	public void setTempMap(double[][] inputMap) {
 
 		temporaryMap = inputMap;
 	}
@@ -139,21 +162,20 @@ public class Storage {
 		return this.initialMap;
 	}
 
-
-
 	/**
 	 * Gets the current map state.
+	 * 
 	 * @return The current map state.
 	 */
-	public double[][] getTempMap(){
+	public double[][] getTempMap() {
 
 		return temporaryMap;
 	}
 
 	/**
-	 * Resets the temporary map to the state of the initially loaded initialMap. 
+	 * Resets the temporary map to the state of the initially loaded initialMap.
 	 */
-	public void resetMap(){
+	public void resetMap() {
 		cancelModelLoop = true;
 		for (int i = 0; i < initialMap.length; i++) {
 			for (int j = 0; j < initialMap[0].length; j++) {
@@ -168,49 +190,58 @@ public class Storage {
 	 * @return An Image of initialMap
 	 */
 	public BufferedImage getDataAsImage() {
-		//Only works if there is data stored.
+		// Only works if there is data stored.
 		if (temporaryMap != null) {
-			// Copies the data into a 1D array to allow the creation of an Image. 
-			double[] temporaryMap1D= get1DArray();
-			// Stores the colour of each pixel in the array/image. 
+			// Copies the data into a 1D array to allow the creation of an
+			// Image.
+			double[] temporaryMap1D = get1DArray();
+			// Stores the colour of each pixel in the array/image.
 			int[] pixels = new int[temporaryMap1D.length];
 
 			for (int i = 0; i < pixels.length; i++) {
 				int value = (int) temporaryMap1D[i];
 
-				// Sets the colour of road pixels(which should be valued 1,2,3 or 4 depending on their N/E/S/W facing direction) to black.
+				// Sets the colour of road pixels(which should be valued 1,2,3
+				// or 4 depending on their N/E/S/W facing direction) to black.
 				// If invert colour is true the road will be white instead.
 				if (value == 1 || value == 2 || value == 3 || value == 4) {
 					Color color;
-					if(!invertColours){
+					if (!invertColours) {
 						color = new Color(0, 0, 0);
-					} else{
+					} else {
 						color = new Color(255, 255, 255);
 					}
 
 					pixels[i] = color.getRGB();
 				}
 
-				// Sets the colour of vehicles(which should be valued the value of the road tile they are on + 9) to blue.
-				// i.e a north facing road (which has a value of 1) would have the value of 10 if a vehicle was on it.
-				else if (value == 10 || value == 11 || value == 12 || value == 13) {
+				// Sets the colour of vehicles(which should be valued the value
+				// of the road tile they are on + 9) to blue.
+				// i.e a north facing road (which has a value of 1) would have
+				// the value of 10 if a vehicle was on it.
+				else if (value == 10 || value == 11 || value == 12
+						|| value == 13) {
 
 					Color color = new Color(0, 0, 255);
 					pixels[i] = color.getRGB();
-				} 
+				}
 
-				// Sets the colour of the traffic lights(which should be valued 100) and the two adjacent tiles(which are valued as 200) to red.
-				else if (value > 100){
+				// Sets the colour of the traffic lights(which should be valued
+				// 100) and the two adjacent tiles(which are valued as 200) to
+				// red.
+				else if (value > 100) {
 
 					Color color = new Color(255, 0, 0);
 					pixels[i] = color.getRGB();
 				} else {
 					Color color;
 
-					if(!invertColours){
-						// Sets every other pixel (0 / empty spaces) to white. If invert colours is true it they will be set to black.
+					if (!invertColours) {
+						// Sets every other pixel (0 / empty spaces) to white.
+						// If invert colours is true it they will be set to
+						// black.
 						color = new Color(255, 255, 255);
-					} else{
+					} else {
 						color = new Color(0, 0, 0);
 					}
 					pixels[i] = color.getRGB();
@@ -219,8 +250,9 @@ public class Storage {
 
 			// The next bit creates the image using the pixel array.
 
-			MemoryImageSource memSource = new MemoryImageSource(temporaryMap[0].length,
-					temporaryMap.length, pixels, 0, temporaryMap[0].length);
+			MemoryImageSource memSource = new MemoryImageSource(
+					temporaryMap[0].length, temporaryMap.length, pixels, 0,
+					temporaryMap[0].length);
 			// Creates a panel for the image to sit on.
 			Panel panel = new Panel();
 			Image image = panel.createImage(memSource);
@@ -237,30 +269,38 @@ public class Storage {
 	}
 
 	/**
-	 * Moves the data from temporaryMap into a 1D array - allows it to be turned into an image.
+	 * Moves the data from temporaryMap into a 1D array - allows it to be turned
+	 * into an image.
 	 * 
 	 * @return A 1D array.
 	 */
 	private double[] get1DArray() {
 
-		// Holds the new data temporarily. 
-		double[] tempArray = new double[temporaryMap.length * temporaryMap[0].length];
-		// Cycles through each element in the 2D temporaryMap array and copies it to tempArray. 
+		// Holds the new data temporarily.
+		double[] tempArray = new double[temporaryMap.length
+				* temporaryMap[0].length];
+		// Cycles through each element in the 2D temporaryMap array and copies
+		// it to tempArray.
 		for (int i = 0; i < temporaryMap.length; i++) {
 			for (int j = 0; j < temporaryMap[0].length; j++) {
-				tempArray[j + (i * (temporaryMap[0].length))] = temporaryMap[i][j];
+				tempArray[j
+						+ (i * (temporaryMap[0].length))] = temporaryMap[i][j];
 
 			}
 		}
-		// Returns the 1D array. 
+		// Returns the 1D array.
 		return tempArray;
 	}
 
-	// This method turns an image into a buffered image. Code was obtained from http://www.rgagnon.com/javadetails/java-0601.html
+	/**
+	 * This method turns an image into a buffered image. Code was obtained from
+	 *
+	 * http://www.rgagnon.com/javadetails/java-0601.html
+	 */
 	public BufferedImage imageToBufferedImage(Image im) {
 
-		BufferedImage bi = new BufferedImage
-				(im.getWidth(null),im.getHeight(null),BufferedImage.TYPE_INT_RGB);
+		BufferedImage bi = new BufferedImage(im.getWidth(null),
+				im.getHeight(null), BufferedImage.TYPE_INT_RGB);
 		Graphics bg = bi.getGraphics();
 		bg.drawImage(im, 0, 0, null);
 		bg.dispose();
@@ -270,41 +310,45 @@ public class Storage {
 	/**
 	 * Toggles the invert colours boolean.
 	 */
-	public void invertColours(){
+	public void invertColours() {
 		invertColours = !invertColours;
 	}
 
 	/**
 	 * Sets the number of vehicles in the currently stored temporary map.
+	 * 
 	 * @return
 	 */
-	public void setNumOfVehicles(int num){
+	public void setNumOfVehicles(int num) {
 
 		numOfVehicles = num;
 	}
 
 	/**
 	 * Gets the number of vehicles in the currently stored temporary map.
+	 * 
 	 * @return
 	 */
-	public int getNumOfVehicles(){
-		
-			return numOfVehicles;
+	public int getNumOfVehicles() {
+
+		return numOfVehicles;
 	}
 
 	/**
-	 * Sets whether the stored data is undergoing alteration (the model is running).
+	 * Sets whether the stored data is undergoing alteration (the model is
+	 * running).
 	 */
-	public void setRuntime(boolean input){
+	public void setRuntime(boolean input) {
 
 		runtime = input;
 	}
 
 	/**
 	 * Gets the runtime state.
+	 * 
 	 * @return Boolean value.
 	 */
-	public boolean getRuntime(){
+	public boolean getRuntime() {
 
 		return runtime;
 	}
@@ -312,55 +356,74 @@ public class Storage {
 	/**
 	 * Stores the percentage of vehicles currently stood still.
 	 */
-	public void setPercentStill(int percent){
+	public void setPercentStill(int percent) {
 
 		percentStoodStill = percent;
 	}
 
 	/**
 	 * Gets the percentage of vehicles currently stood still.
+	 * 
 	 * @return The percentage in double format.
 	 */
-	public int getPercentStill(){
+	public int getPercentStill() {
 
 		return percentStoodStill;
 	}
 
 	/**
-	 * Stores the average distance between each vehicle and the vehicle in front.
+	 * Stores the average distance between each vehicle and the vehicle in
+	 * front.
+	 * 
 	 * @param value
 	 */
-	public void setAverageVehicDist(double value){
+	public void setAverageVehicDist(double value) {
 
 		averageVehicDist = value;
 	}
 
 	/**
 	 * Gets the average distance between each vehicle and the vehicle in front.
+	 * 
 	 * @return Double value.
 	 */
-	public double getAverageVehicDist(){
+	public double getAverageVehicDist() {
 
 		return averageVehicDist;
 	}
-	
-	public void setOverallStill(double input){
-		
+
+	/**
+	 * Sets the overall average percentage of vehicles that were stood still in the model.
+	 * @param input The percentage.
+	 */
+	public void setOverallStill(double input) {
+
 		overallStill = input;
 	}
-	
-	public double getOverallStill(){
-		
+	/**
+	 * Sets the overall average percentage of vehicles that were stood still in the model.
+	 * @return The percentage average value.
+	 */
+	public double getOverallStill() {
+
 		return overallStill;
 	}
-	
-	public void setOverallDist(double input){
-		
+
+	/**
+	 * Sets the overall average distance that was between each vehicle and the vehicle in front.
+	 * @param input Percentage value.
+	 */
+	public void setOverallDist(double input) {
+
 		overallDist = input;
 	}
-	
-	public double getOverallDist(){
-		
+
+	/**
+	 * Gets the overall average distance that was between each vehicle and the vehicle in front.
+	 * @return The percentage average value.
+	 */
+	public double getOverallDist() {
+
 		return overallDist;
 	}
 }
